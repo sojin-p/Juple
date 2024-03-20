3085893286e4b9284a67468206975c919b3add79
Sojin Park <luv2hong@gmail.com>
2024-03-20 17:27:31 +0900

    Initial Commit

---
+++ Juple/JupleApp.swift
@@ -0,0 +1,17 @@
+//
+//  JupleApp.swift
+//  Juple
+//
+//  Created by 박소진 on 2024/03/20.
+//
+
+import SwiftUI
+
+@main
+struct JupleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
