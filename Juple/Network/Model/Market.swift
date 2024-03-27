fbca9615cfd17dec08a3bff08190a2bf7a5f1440
Sojin Park <luv2hong@gmail.com>
2024-03-27 16:49:03 +0900

    [Add] Market 모델 파일 추가

---
+++ Juple/Network/Model/Market.swift
@@ -0,0 +1,20 @@
+//
+//  Market.swift
+//  Juple
+//
+//  Created by 박소진 on 2024/03/27.
+//
+
+import Foundation
+
+struct Market: Codable, Hashable {
+    let market: String
+    let koreanName: String
+    let englishName: String
+    
+    enum CodingKeys: String, CodingKey {
+        case market
+        case koreanName = "korean_name"
+        case englishName = "english_name"
+    }
+}
