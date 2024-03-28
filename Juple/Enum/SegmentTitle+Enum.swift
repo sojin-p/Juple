07e1d626aff866ea20490e9c54795fb37260a26c
Sojin Park <luv2hong@gmail.com>
2024-03-28 18:12:47 +0900

    [Add] SegmentTitle+Enum 파일 추가

---
+++ Juple/Enum/SegmentTitle+Enum.swift
@@ -0,0 +1,15 @@
+//
+//  SegmentTitle+Enum.swift
+//  Juple
+//
+//  Created by 박소진 on 2024/03/28.
+//
+
+import Foundation
+
+enum SegmentTitle: String, CaseIterable {
+    //CoinFilter
+    case krw = "KRW"
+    case btc = "BTC"
+    case usdt = "USDT"
+}
