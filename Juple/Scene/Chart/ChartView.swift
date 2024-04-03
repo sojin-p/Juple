5cd319aba91806902013eb9ba84b427b3351789d
Sojin Park <luv2hong@gmail.com>
2024-04-03 14:57:21 +0900

    [Add] ChartView 파일 추가

---
+++ Juple/Scene/Chart/ChartView.swift
@@ -0,0 +1,24 @@
+//
+//  ChartView.swift
+//  Juple
+//
+//  Created by 박소진 on 2024/04/01.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    
+    @EnvironmentObject var viewModel: CoinlistViewModel
+    
+    let market: Market
+    
+    var body: some View {
+        
+    }
+    
+}
+
+//#Preview {
+//    ChartView()
+//}
